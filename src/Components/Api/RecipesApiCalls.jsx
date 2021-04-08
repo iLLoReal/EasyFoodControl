60eb5d17d73eaa516f344d8895fc1ca0e26582dc
olivier <olivier.laffon@epitech.eu>
2021-04-08 17:31:46 +0200

    Cleaned and optimized RecipesApiCalls functions

--- src/Components/Api/RecipesApiCalls.jsx
+++ src/Components/Api/RecipesApiCalls.jsx
@@ -3,16 +3,17 @@ import ApiMethods from './ApiMethods';
 
 const RecipesApiCalls = {
   sendRecipes: async (recipes, recipeId, token) => {
-    if (!token || !token.length)
-      token = undefined;
-    const recipesResponse = await ApiMethods.getPostResultAlpha(routes.recipesRoute, {recipeId: recipeId, recipes: recipes, token: token}, null);
-    if (recipesResponse.data !== undefined && recipesResponse.data !== 'No recipes') {
+    const data = {
+      recipes: recipes,
+      recipeId: recipeId,
+      token: (!token || !token.length ) ? undefined : token,
+    };
+    const recipesResponse = await ApiMethods.getPostResultAlpha(routes.recipesRoute, data, null);
+    if (recipesResponse?.data !== undefined && recipesResponse.data !== 'No recipes') {
       return recipesResponse.data;
     }
-    else {
-      console.log(ApiMethods.getErrorResponse(recipesResponse));
-      return null;
-    }
+    console.log(ApiMethods.getErrorResponse(recipesResponse));
+    return null;
   }
 }
 export default RecipesApiCalls;
