06e2e0f8b453aee17b7ec8b0202fce777f04053d
olivier <olivier.laffon@epitech.eu>
2021-04-08 16:51:54 +0200

    Added check on null verifyTokenResponse in GeneralApiCalls

--- src/Components/Api/GeneralApiCalls.jsx
+++ src/Components/Api/GeneralApiCalls.jsx
@@ -4,7 +4,7 @@ import * as routes from '../Routes';
 const GeneralApiCalls = {
     verifyToken: async (token) => {
         const verifyTokenRepsonse = await ApiMethods.getPostResultAlpha(routes.verifyTokenRoute, {token: token}, null);
-        if (verifyTokenRepsonse.status !== undefined) {
+        if (verifyTokenRepsonse?.status !== undefined) {
            return verifyTokenRepsonse.status;
         } else {
             console.log(ApiMethods.getErrorResponse(verifyTokenRepsonse));
